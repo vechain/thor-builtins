6eb0ef72269346394b7202fa33506e03dacda5fd
linglong <linglong.yan@vechain.com>
2018-07-17 16:32:27 +0800

    add comments to contract params

--- contracts/params.sol
+++ contracts/params.sol
@@ -1,5 +1,11 @@
 pragma solidity ^0.4.23;
 
+/// @title Params stores the governance params of VeChain thor.
+/// The params can be set by executor which is a contract and authorized to modify governance params by a voting Committee.
+/// Any one can get the params just by calling "get" funtion.
+/// The governance params is written to genesis block at launch time.
+/// You can check these params at source file: https://github.com/vechain/thor/blob/master/thor/params.go 
+
 interface Params {
     function executor() external view returns(address);
     function set(bytes32 _key, uint256 _value) external;
