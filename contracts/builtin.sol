885307d512b208900b9f3d016519c4676c250246
linglong <linglong.yan@vechain.com>
2018-07-18 16:36:03 +0800

    add comments to builtin.sol

--- contracts/builtin.sol
+++ contracts/builtin.sol
@@ -7,97 +7,259 @@ import "./extension.sol";
 import "./params.sol";
 import "./executor.sol";
 
+/**
+ * @title Builtin
+ * @dev The VeChainThor builtin contracts are encapsulated in library Builtin.
+ * It's very easy to use it for the developer, just need import it.
+ */
 library Builtin {
+
+    /// @return the instance of the contract "Authority".
     function getAuthority() internal pure returns(Authority) {
         return Authority(uint160(bytes9("Authority")));
     }
 
+    /// @return the instance of the contract "Energy".
     function getEnergy() internal pure returns(Energy) {
         return Energy(uint160(bytes6("Energy")));
     }
 
+    /// @return the instance of the contract "Extension".
     function getExtension() internal pure returns(Extension) {
         return Extension(uint160(bytes9("Extension")));
     }
 
+    /// @return the instance of the contract "Params".
     function getParams() internal pure returns(Params) {
         return Params(uint160(bytes6("Params")));
     }
 
+    /// @return the instance of the contract "Executor".
     function getExecutor() internal pure returns(Executor) {
         return Executor(uint160(bytes6("Executor")));
     }
 
 
-    // 
-    // energy
-    //
     Energy constant energy = Energy(uint160(bytes6("Energy")));
+
+    /// @return amount of VeThor in account 'self'.
     function $energy(address self) internal view returns(uint256 amount){
         return energy.balanceOf(self);
     }
 
+    /// @notice transfer 'amount' of VeThor from msg sender to account 'self'.
     function $transferEnergy(address self, uint256 amount) internal{
         energy.transfer(self, amount);
     }
 
+    /// @notice transfer 'amount' of VeThor from account 'self' to account 'to'.
     function $moveEnergyTo(address self, address to, uint256 amount) internal{
         energy.move(self, to, amount);
     }
 
-    //
-    // prototype
-    //
     Prototype constant prototype = Prototype(uint160(bytes9("Prototype")));
+
+    /// @return master address of self
     function $master(address self) internal view returns(address){
         return prototype.master(self);
     }
+
+    /// @notice 'newMaster' will be set to contract 'self' and this function only works when msg sender is the old master.
     function $setMaster(address self, address newMaster) internal {
         prototype.setMaster(self, newMaster);
     }
+
+    /// @return the amount of VET at blockNumber
     function $balance(address self, uint blockNumber) internal view returns(uint256){
         return prototype.balance(self, blockNumber);
     }
+
+    /// @return the amount of energy at blockNumber
     function $energy(address self, uint blockNumber) internal view returns(uint256){
         return prototype.energy(self, blockNumber);
     }
+
+    /// @notice 'self' check if address self is a contract account
     function $hasCode(address self) internal view returns(bool){
         return prototype.hasCode(self);
     }
+
+    /// @return value indexed by key at self storage
     function $storageFor(address self, bytes32 key) internal view returns(bytes32){
         return prototype.storageFor(self, key);
     }
+
+    /// @return the creditPlan of contract 'self'
     function $creditPlan(address self) internal view returns(uint256 credit, uint256 recoveryRate){
         return prototype.creditPlan(self);
     }
+
+    /// @notice set creditPlan to contract 'self'
     function $setCreditPlan(address self, uint256 credit, uint256 recoveryRate) internal{
         prototype.setCreditPlan(self, credit, recoveryRate);
     }
+
+    /// @notice check if address 'user' is the user of contract 'self'.
     function $isUser(address self, address user) internal view returns(bool){
         return prototype.isUser(self, user);
     }
+
+    /// @notice return the current credit of 'user' of the contract 'self'.
     function $userCredit(address self, address user) internal view returns(uint256){
         return prototype.userCredit(self, user);
     }
+
+    /// @notice add address 'user' to the user list of a contract 'self'.
     function $addUser(address self, address user) internal{
         prototype.addUser(self, user);
     }
+
+    /// @notice remove 'user' from the user list of a contract 'self'.
     function $removeUser(address self, address user) internal{
         prototype.removeUser(self, user);
     }
+
+    /// @notice msg sender volunteers to be a sponsor of the contract 'self'.
     function $sponsor(address self) internal{
         prototype.sponsor(self);
     }
+
+    /// @notice msg sender removes itself from the sponsor candidates list of contract 'self'.
     function $unsponsor(address self) internal {
         prototype.unsponsor(self);
     }
+
+    /// @notice check if 'sponsorAddress' is the sponser of contract 'self'.
     function $isSponsor(address self, address sponsor) internal view returns(bool){
         return prototype.isSponsor(self, sponsor);
     }
+
+    /// @notice select 'sponsorAddress' to be current selected sponsor of contract 'self'
     function $selectSponsor(address self, address sponsor) internal{
         prototype.selectSponsor(self, sponsor);
     }
+
+    /// @notice return current selected sponsor of contract 'self'
     function $currentSponsor(address self) internal view returns(address){
         return prototype.currentSponsor(self);
+    }
+
+    Authority constant authority = Authority(uint160(bytes9("Authority")));
+
+    /// @return executor address
+    function executor() internal view returns(address) {
+        return authority.executor();
+    }
+
+    /// @notice add a proposer to the candidates lists.
+    function add(address _signer, address _endorsor, bytes32 _identity) internal {
+        return authority.add(_signer, _endorsor, _identity);
+    }
+
+    /// @notice remove proposer '_signer' from the candidates lists.
+    function revoke(address _signer) internal {
+        return authority.revoke(_signer);
+    }
+
+    /// @notice get infomation about proposer "_signer"
+    function get(address _signer) internal view returns(bool listed, address endorsor, bytes32 identity, bool active) {
+        return authority.get(_signer);
+    }
+
+    /// @notice get the first proposer in the candidates list.
+    function first() internal view returns(address) {
+        return authority.first();
+    }
+
+    /// @notice get next one of proposer "_signer"
+    function next(address _signer) internal view returns(address) {
+        return authority.next(_signer);
+    }
+
+    Executor constant executor = Executor(uint160(bytes6("Executor")));
+
+    function propose(address _target, bytes _data) internal returns(bytes32) {
+        return executor.propose(_target, _data);
+    }
+
+    function approve(bytes32 _proposalID) internal {
+        return executor.approve(_proposalID);
+    }
+
+    function execute(bytes32 _proposalID) internal {
+        return executor.execute(_proposalID);
+    }
+
+    function addApprover(address _approver, bytes32 _identity) internal {
+        return executor.addApprover(_approver, _identity);
+    }
+
+    function revokeApprover(address _approver) internal {
+        return executor.revokeApprover(_approver);
+    }
+
+    function attachVotingContract(address _contract) internal {
+        return executor.attachVotingContract(_contract);
+    }
+
+    function detachVotingContract(address _contract) internal {
+        return executor.detachVotingContract(_contract);
+    }
+
+    Extension constant extension = Extension(uint160(bytes9("Extension")));
+
+    /// @notice blake2b256 computes blake2b-256 checksum for given data.
+    function blake2b256(bytes _value) internal view returns(bytes32) {
+        return extension.blake2b256(_value);
+    }
+
+    /// @notice These functions return corresponding block info from genesis to best block.
+    function blockID(uint num) internal view returns(bytes32) {
+        return extension.blockID(num);
+    }
+ 
+    function blockTotalScore(uint num) internal view returns(uint64) {
+        return extension.blockTotalScore(num);
+    }
+
+    function blockTime(uint num) internal view returns(uint) {
+        return extension.blockTime(num);
+    }
+
+    function blockSigner(uint num) internal view returns(address) {
+        return extension.blockSigner(num);
+    }
+
+    /// @return total supply of VET
+    function totalSupply() internal view returns(uint256) {
+        return extension.totalSupply();
+    }
+
+    /// @notice These funtions return corresponding current transaction info.
+    function txProvedWork() internal view returns(uint256) {
+        return extension.txProvedWork();
+    }
+    function txID() internal view returns(bytes32) {
+        return extension.txID();
+    }
+    function txBlockRef() internal view returns(bytes8) {
+        return extension.txBlockRef();
+    }
+
+    function txExpiration() internal view returns(uint) {
+        return extension.txExpiration();
+    }
+
+    Params constant params = Params(uint160(bytes6("Params")));
+    function executor() internal view returns(address) {
+        return params.executor();
+    }
+ 
+    function set(bytes32 _key, uint256 _value) internal {
+        return params.set(_key, _value);
+    }
+
+    function get(bytes32 _key) internal view returns(uint256) {
+        return params.get(_key);
     }
 }
